4f1d6f399d701e386579f11d16745d067b9dce8e
Bang <hb398@cornell.edu>
2018-05-19 11:42:29 -0700

    Modifies the number of instruments to match the smap problem

--- java/src/main/java/ifeed/problem/eoss/EOSSParams.java
+++ java/src/main/java/ifeed/problem/eoss/EOSSParams.java
@@ -14,10 +14,10 @@ public class EOSSParams {
 
     public static boolean tallMatrix = false;
 
-    public static String[] instrument_list = {"ACE_ORCA","ACE_POL","ACE_LID","CLAR_ERB","ACE_CPR","DESD_SAR","DESD_LID","GACM_VIS","GACM_SWIR","HYSP_TIR","POSTEPS_IRS","CNES_KaRIN"};
-    public static String[] orbit_list = {"LEO-600-polar-NA", "SSO-600-SSO-AM", "SSO-600-SSO-DD","SSO-800-SSO-DD", "SSO-800-SSO-PM"};
+    //public static String[] instrument_list = {"ACE_ORCA","ACE_POL","ACE_LID","CLAR_ERB","ACE_CPR","DESD_SAR","DESD_LID","GACM_VIS","GACM_SWIR","HYSP_TIR","POSTEPS_IRS","CNES_KaRIN"};
+    //public static String[] orbit_list = {"LEO-600-polar-NA", "SSO-600-SSO-AM", "SSO-600-SSO-DD","SSO-800-SSO-DD", "SSO-800-SSO-PM"};
 
-    public static int num_instruments = 12;
+    public static int num_instruments = 5;
     public static int num_orbits = 5;
     
     
